e6070126035c1c6850d50be4c3c1916b20aa87f9
NukiCoockie <nimenithewho@gmail.com>
2021-07-27 13:25:05 +0500

    5.5. Простое число [#171694 #182247]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,16 +2,12 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
+        boolean prime = number > 1;
         for (int index = 2; index < number; index++) {
             if (number % index == 0) {
                 prime = false;
                 break;
             }
-        }
-        if (number == 1) {
-            prime = false;
-            System.out.println("В математике принято не относить единицу ни к простым, ни к составным числам.");
         }
         return prime;
     }
