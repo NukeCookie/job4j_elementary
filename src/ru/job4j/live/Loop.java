e66e632f82e96a8c46c5cb53035f9614be2af33c
NukiCoockie <nimenithewho@gmail.com>
2021-07-27 21:15:47 +0500

    5.9.1. Генерация конструкций в IDEA [#279134]

--- src/ru/job4j/live/Loop.java
+++ src/ru/job4j/live/Loop.java
@@ -2,9 +2,4 @@ package ru.job4j.live;
 
 public class Loop {
 
-    public static void main(String[] args) {
-        for (int i = 0; i < 11; i++) {
-            System.out.println(i);
-        }
-    }
 }
--- src/ru/job4j/live/Main.java
+++ src/ru/job4j/live/Main.java
@@ -1,7 +1,5 @@
 package ru.job4j.live;
 
 public class Main {
-    public static void main(String[] args) {
 
-    }
 }
