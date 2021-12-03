6c04a34ea91ac2f8ad71df6e2598ae548ca793f3
NukeCookie <nimenithewho@gmail.com>
2021-12-03 20:28:11 +0500

    3. Перегрузить метод max для трех чисел [#122644]

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -7,27 +7,67 @@ import org.junit.Test;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int first = 1;
+        int second = 2;
+        int result = Max.max(first, second);
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax3To1Then3() {
-        int left = 3;
-        int right = 1;
-        int result = Max.max(left, right);
+        int first = 3;
+        int second = 1;
+        int result = Max.max(first, second);
         int expected = 3;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax3To3Then3() {
-        int left = 3;
-        int right = 3;
-        int result = Max.max(left, right);
+        int first = 3;
+        int second = 3;
+        int result = Max.max(first, second);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax3To3To3Then3() {
+        int first = 3;
+        int second = 3;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax3To2To1Then3() {
+        int first = 3;
+        int second = 2;
+        int third = 1;
+        int result = Max.max(first, second, third);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax2To3To1Then3() {
+        int first = 2;
+        int second = 3;
+        int third = 1;
+        int result = Max.max(first, second, third);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1To2To3Then3() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int result = Max.max(first, second, third);
         int expected = 3;
         Assert.assertEquals(result, expected);
     }
