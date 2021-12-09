ee3c751e0ff1981086098b72e9838a85b3a37aa1
NukeCookie <nimenithewho@gmail.com>
2021-12-09 18:06:16 +0500

    3. Перегрузить метод max для трех чисел [#122644 #235589]

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -71,5 +71,60 @@ public class MaxTest {
         int expected = 3;
         Assert.assertEquals(result, expected);
     }
+
+    @Test
+    public void whenMax1To2To3To4Then4() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int fourth = 4;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 4;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1To2To4To3Then4() {
+        int first = 1;
+        int second = 2;
+        int third = 4;
+        int fourth = 3;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 4;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1To4To2To3Then4() {
+        int first = 1;
+        int second = 4;
+        int third = 2;
+        int fourth = 3;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 4;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax4To1To2To3Then4() {
+        int first = 4;
+        int second = 1;
+        int third = 2;
+        int fourth = 3;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 4;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1To1To1To1Then1() {
+        int first = 1;
+        int second = 1;
+        int third = 1;
+        int fourth = 1;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 1;
+        Assert.assertEquals(result, expected);
+    }
 }
 
